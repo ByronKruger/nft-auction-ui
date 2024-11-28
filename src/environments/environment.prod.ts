3f08a8fca84e8352be51851ec81a6e50a1dabad4
ByronKruger <byronkruger3@gmail.com>
2024-11-28 09:08:15 +0200

    Added prod build instructions and prod env file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: "https://nft-auction-2-gsaphmadgpewgef7.canadacentral-01.azurewebsites.net/api/",
+    hubUrl: "https://nft-auction-2-gsaphmadgpewgef7.canadacentral-01.azurewebsites.net/hubs/",
+}
